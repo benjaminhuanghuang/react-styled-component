b6b90f2a3e14e1b353201a593951a378cfcd96e0
benjaminhuanghuang <bhuang@rms.com>
2020-12-12 01:08:49 -0800

    upate

--- src/App.js
+++ src/App.js
@@ -1,17 +1,27 @@
 import React from "react";
 
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import styled from 'styled-components/macro'
+
 //
 import { Home, About } from "./pages";
 
+
+const MainWrapper = styled.div`
+  width: 100%;
+  max-width: 1200px;
+  margin: 0 auto
+`;
 function App() {
   return (
+    <MainWrapper>
     <Router>
       <Switch>
         <Route path="/" exact component ={Home}/>
         <Route path="/" exact component ={Home}/>
       </Switch>
     </Router>
+    </MainWrapper>
   );
 }
 
